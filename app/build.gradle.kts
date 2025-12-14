0dc7eb0e243340638f725d36ee7833b5841bc7f9
Lua <maui_195@hotmail.com>
2025-12-14 14:23:08 +0100

    Metadata and preparations for fdroid release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.myslog"
         minSdk = 27
         targetSdk = 36
-        versionCode = 1043
-        versionName = "1.0.43"
+        versionCode = 1051
+        versionName = "1.0.51"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
